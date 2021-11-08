22d93eaa35ec27b06016de4f08183c644da1a0b9
enahas <tefinahas@gmail.com>
2021-11-08 19:25:42 -0300

    nav

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import { Fragment } from 'react';
+import './components/Header/Header';
+import Header from './components/Header/Header';
+
+const App = () =>{
+  return(
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
+}
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import './Header.css'
+import logo_ATR from './logo_ATR.png'
+
+const Header = () =>{
+    return(
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light
+                            bg-transparent">
+                        <a className="navbar-brand" href="#"><img src={logo_ATR} alt="logo"
+                                className="logoHeader" /></a>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Inicio</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="pages/atr.html">ATR</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="pages/servicios.html">Servicios</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="pages/contacto.html">Contacto</a>
+                                </li>
+                            </ul>
+                        </div>
+            </nav>
+        </header>
+    )
+}
+export default Header
