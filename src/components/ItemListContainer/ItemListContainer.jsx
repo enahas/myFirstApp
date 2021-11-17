bdd593d7799c6cfcde023870f2e9ac32c109b304
enahas <tefinahas@gmail.com>
2021-11-17 10:15:31 -0300

    itemList v2

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,26 @@
 import './ItemListContainer.css'
+import ItemList from './components/ItemList/ItemList';
 
 const ItemListContainer = (props) =>{
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      const list = [{ title: services }];
+      
+      const getItems = new Promise((resolve) => {
+        setTimeout(() => {resolve(list);}, 2000);
+      });
+      getItems.then((res) => {
+        setItems(res);
+      });
+    }, []);
+  
+    
     return(
-        <h1>{props.greetings}</h1>
+        <div>
+            <h1>{props.greetings}</h1>
+            <ItemList items={items} />
+        </div>
     )
 }
 export default ItemListContainer
