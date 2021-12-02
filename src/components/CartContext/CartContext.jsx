18614747d7e4f49460ac5a7ca37787c4e9b88fdb
enahas <tefinahas@gmail.com>
2021-12-02 12:38:34 -0300

    Context v3

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -6,28 +6,23 @@ const Context=React.createContext()
 
 export function UserContext({children}){
     const[serviciosGuardados, setServiciosGuardados] = useState([])
-
-    // useEffect(()=>{
-    //     guardarServicios()
-    // }, [])
     
     const guardarServicios = (serviciosVenta, value) => {
         const found = serviciosGuardados.find(element => 
             element.Nombre === serviciosVenta.name
-            // console.log(element.Nombre, serviciosVenta.name)
-        )
-        console.log(found)
+        );
+        
         if (!found){
             setServiciosGuardados([...serviciosGuardados,{'Nombre':serviciosVenta.name, 'Precio':serviciosVenta.price, 'Cantidad':value}])
         }
         else{return}     
-    }
+    };
 
     return(
         <Context.Provider value={{ serviciosGuardados, guardarServicios }}>
             {children}
         </Context.Provider>
-    )
+    );
 }
 
 export default Context
