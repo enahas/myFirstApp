ff804d17f60e4b977ca289d441464a91d8c14054
enahas <tefinahas@gmail.com>
2021-12-17 10:56:01 -0300

    Firebase II v1

--- src/components/Context/ProductContext.jsx
+++ src/components/Context/ProductContext.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { getDocs, collection, getFirestore, addDoc } from 'firebase/firestore';
 
 const ProductContext=React.createContext()
 
@@ -6,25 +8,54 @@ const CartFuncion = ({ children }) => {
     const [cart, setCart] = useState([]);
     const [unidades, setUnidades] = useState(0);
     const [total, setTotal] = useState(0);
-  
+    const [isLoading, setIsLoading] = useState(true);
+    const db = getFirestore()
+    const ref = collection(db, 'services')
+    const refCart = collection(db, 'cartItems')
+
+    useEffect(() => {
+      getDocs(ref)
+      .then((snapShot) => {
+        setUnidades(snapShot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+        // snapShot.docs.map((service) => setUnidades(prev=>([...prev, service.data()])))
+        setIsLoading(false)
+      })
+
+      getDocs(refCart)
+      .then((snapShot) => {
+        setCart(snapShot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+        setIsLoading(false)
+      })
+    }, [])
+
+    const getCardItems = () => {
+      getDocs(refCart)
+      .then((snapShot) => {
+        setCart(snapShot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+        setIsLoading(false)
+      })
+    }
+
     const onAdd = (producto, cantidad) => {
-      const itemExiste = cart.find((item) => item.id === producto.serviciosVenta.id);
-      // console.log(producto.price)
+      const itemExiste = cart.find((item) => item.serviciosVenta.id === producto.serviciosVenta.id);
+      // console.log(producto.serviciosVenta.id)
       if (!itemExiste) {
-        setCart([
-          ...cart,
-          {
-            ...producto,
-            cantidad: cantidad,
-            subtotal: producto.serviciosVenta.price * cantidad,
-          },
-        ]);
-        setTotal(total + producto.serviciosVenta.price * cantidad);
+        addDoc(refCart, producto.serviciosVenta)
+        getCardItems()
+        // setCart([
+        //   ...cart,
+        //   {
+        //     ...producto,
+        //     cantidad: cantidad,
+        //     subtotal: producto.serviciosVenta.price * cantidad,
+        //   },
+        // ]);
+        // setTotal(total + producto.serviciosVenta.price * cantidad);
   
-        setUnidades(unidades + cantidad);
+        // setUnidades(unidades + cantidad);
       } else {
         const cartAux = cart.map((item) => {
-          if (item.id === producto.serviciosVenta.id) {
+          if (item.serviciosVenta.id === producto.serviciosVenta.id) {
             item.cantidad += cantidad;
             item.subtotal += producto.serviciosVenta.price * cantidad;
           }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,62 @@ import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const data = [
+  {
+    name: "Coaching ontológico: sesión personal",
+    price: 5000,
+    img: "/assets/img/servicio1.jpg",
+    adress: '/category/A',
+    item: '/item/Coaching',
+    category: 'A',
+    descripcion: "En este camino, yo te acompañaré con distintas herramientas y técnicas, siendo la más importante la pregunta, para instarte a reflexionar y que puedas descubrir fortalezas, debilidades y oportunidades encontrando por vos mismo la/s respuesta/s que te permitan transitar de tu situación actual a la situación deseada, aquella en que tu objetivo o meta se encuentra cumplido.",
+    stock: 10,
+    count: 0,
+},
+{
+    name:"Mentoría de propósito para jóvenes",
+    price: 1000,
+    img: "/assets/img/servicio3.jpg",
+    adress: '/category/A',
+    item: '/item/Mentoría',
+    category: 'A',
+    descripcion: "La Mentoría de Propósito para Jóvenes está compuesta de seis (6) encuentros de dos (2) horas máximo cada uno, con una duración total de un (1) mes.",
+    stock: 7,
+    count: 0,
+},
+{
+    name:"Sesión de lectura de mapa personal de eneagrama",
+    price: 3500,
+    img: "/assets/img/servicio4.png",
+    adress: '/category/B',
+    item: '/item/Eneagrama',
+    category: 'B',
+    descripcion: "Es un proceso uno a uno en el que como facilitadora en Eneagrama te acompañaré a explorar el símbolo y sus interrelaciones para que descubras por vos mismo tu esencia. Como siempre digo, el autoconocimiento es un camino que transitamos toda vida. Este puede ser tu inicio, el momento donde tomes la punta del ovillo y comiences a desandar.",
+    stock: 15,
+    count: 0,
+},
+{
+    name:"Workshop de eneagrama",
+    price: 1500,
+    img: "/assets/img/servicio2.png",
+    adress: '/category/B',
+    item: '/item/Workshop',
+    category: 'B',
+    descripcion: "En este curso vas a comenzar a explorar la sabiduría del Eneagrama y todo su potencial como herramienta de autoconocimiento: qué es, cuál es su origen, para que sirve, cómo podes utilizarlo en tu camino de evolución personal y profesional así como también para mejorar tu relación con vos mismo y tu entorno.",
+    stock: 5,
+    count: 0,
+}
+]
+
+const db = getFirestore()
+
+const ref = collection(db, 'services')
+
+data.map(service => addDoc(ref, service))
 
 ReactDOM.render(
   <React.StrictMode>
